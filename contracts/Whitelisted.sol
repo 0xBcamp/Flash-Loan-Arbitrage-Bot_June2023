34bc269abf51442909d43543c4f622bb482eeae7
Oleg Iaremchuk <oleg.iaremchuk@pm.me>
2023-06-25 21:21:49 +0200

    Implement Whitelist contract for access control

---
+++ contracts/Whitelisted.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Whitelisted {
+    mapping(address => bool) private whitelist;
+    address private owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can call this function.");
+        _;
+    }
+
+    modifier onlyWhitelisted() {
+        require(
+            whitelist[msg.sender],
+            "Only whitelisted addresses can call this function."
+        );
+        _;
+    }
+
+    function addToWhitelist(address _address) external onlyOwner {
+        whitelist[_address] = true;
+    }
+
+    function removeFromWhitelist(address _address) external onlyOwner {
+        whitelist[_address] = false;
+    }
+
+    function isWhitelisted(address _address) public view returns (bool) {
+        return whitelist[_address];
+    }
+}

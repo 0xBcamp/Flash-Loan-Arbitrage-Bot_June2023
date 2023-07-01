610e8c4bd233f81a79210db13d55da590f458aeb
Oleg Iaremchuk <oleg.iaremchuk@pm.me>
2023-07-01 09:45:33 +0200

    Add deployment network configuration

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,7 +1,17 @@
 import { HardhatUserConfig } from "hardhat/config";
+import "dotenv/config";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.20",
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545",
+    },
+    optimismGoerli: {
+      url: `${process.env.ALCHEMY_GOERLI_URL}`,
+      accounts: [`0x${process.env.GOERLI_PRIVATE_KEY}`],
+    },
+  },
 };
 
 export default config;

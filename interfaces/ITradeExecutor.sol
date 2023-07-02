6fa4f46633f7d87e97a302be99d717ad04bc9f2b
Oleg Iaremchuk <oleg.iaremchuk@pm.me>
2023-07-02 14:28:00 +0200

    Downgrade Solidity to 0.8.19

--- interfaces/ITradeExecutor.sol
+++ interfaces/ITradeExecutor.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+pragma solidity ^0.8.19;
 
 interface ITradeExecutor {
     function executeTrade(address dex, address token, uint256 amount) external;

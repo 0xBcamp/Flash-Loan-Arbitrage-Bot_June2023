286ac383bd8edc942a0fc49c1144285d1497f046
Oleg Iaremchuk <oleg.iaremchuk@pm.me>
2023-07-20 12:25:29 +0200

    Do not hardcode trading amount, use reserves

--- contracts/contracts/ArbitrageFinder.sol
+++ contracts/contracts/ArbitrageFinder.sol
@@ -35,60 +35,100 @@ contract ArbitrageFinder is IArbitrageFinder, Whitelisted {
         onlyWhitelisted
         returns (bool, Arbitrage.Opportunity memory)
     {
-        Arbitrage.Opportunity memory arbitrage;
         uint256 uniswapPrice = getUniswapPrice(token1, token2);
         uint256 sushiswapPrice = getSushiSwapPrice(token1, token2);
 
         if (uniswapPrice > 0 && sushiswapPrice > 0) {
+            uint256 uniswapTokenBalance = getEffectiveTokenBalance(
+                uniswapRouterAddr,
+                token1,
+                token2
+            );
+            uint256 sushiSwapTokenBalance = getEffectiveTokenBalance(
+                sushiSwapRouterAddr,
+                token1,
+                token2
+            );
+
+            uint256 effectiveTokenBalance = uniswapTokenBalance <
+                sushiSwapTokenBalance
+                ? uniswapTokenBalance
+                : sushiSwapTokenBalance;
+
+            uint256 tradeAmount = (effectiveTokenBalance * 10) / 100;
+
             if (uniswapPrice > sushiswapPrice) {
-                if (isArbitrageEligable(uniswapPrice, sushiswapPrice)) {
-                    arbitrage = Arbitrage.Opportunity(
-                        Arbitrage.Transaction(
-                            token1,
-                            token2,
-                            sushiSwapRouterAddr,
-                            10
-                        ),
-                        Arbitrage.Transaction(
-                            token2,
-                            token1,
-                            uniswapRouterAddr,
-                            0
-                        )
-                    );
+                if (
+                    isArbitrageEligable(
+                        uniswapPrice,
+                        sushiswapPrice,
+                        tradeAmount
+                    )
+                ) {
+                    Arbitrage.Opportunity memory arbitrage = Arbitrage
+                        .Opportunity(
+                            Arbitrage.Transaction(
+                                token1,
+                                token2,
+                                sushiSwapRouterAddr,
+                                tradeAmount
+                            ),
+                            Arbitrage.Transaction(
+                                token2,
+                                token1,
+                                uniswapRouterAddr,
+                                0
+                            )
+                        );
                     return (true, arbitrage);
                 }
             } else {
-                if (isArbitrageEligable(sushiswapPrice, uniswapPrice)) {
-                    arbitrage = Arbitrage.Opportunity(
-                        Arbitrage.Transaction(
-                            token1,
-                            token2,
-                            uniswapRouterAddr,
-                            10
-                        ),
-                        Arbitrage.Transaction(
-                            token2,
-                            token1,
-                            sushiSwapRouterAddr,
-                            0
-                        )
-                    );
+                if (
+                    isArbitrageEligable(
+                        sushiswapPrice,
+                        uniswapPrice,
+                        tradeAmount
+                    )
+                ) {
+                    Arbitrage.Opportunity memory arbitrage = Arbitrage
+                        .Opportunity(
+                            Arbitrage.Transaction(
+                                token1,
+                                token2,
+                                uniswapRouterAddr,
+                                tradeAmount
+                            ),
+                            Arbitrage.Transaction(
+                                token2,
+                                token1,
+                                sushiSwapRouterAddr,
+                                0
+                            )
+                        );
                     return (true, arbitrage);
                 }
             }
         }
 
-        return (false, arbitrage);
+        return (
+            false,
+            Arbitrage.Opportunity(
+                Arbitrage.Transaction(address(0), address(0), address(0), 0),
+                Arbitrage.Transaction(address(0), address(0), address(0), 0)
+            )
+        );
     }
 
     function isArbitrageEligable(
         uint256 higherPrice,
-        uint256 lowerPrice
+        uint256 lowerPrice,
+        uint256 tradeAmount
     ) private view returns (bool) {
         uint256 priceDifference = ((higherPrice - lowerPrice) * 10000) /
             lowerPrice; // Calculate the price difference as a percentage
-        if (priceDifference >= PRICE_TOLERANCE_PERCENT * 100) {
+        if (
+            priceDifference >= PRICE_TOLERANCE_PERCENT * 100 && tradeAmount > 0
+        ) {
             return true;
         }
         return false;
@@ -120,6 +160,25 @@ contract ArbitrageFinder is IArbitrageFinder, Whitelisted {
         path[1] = token2;
 
         try sushiSwapRouter.getAmountsOut(1e18, path) returns (
+            uint256[] memory amounts
+        ) {
+            return amounts[amounts.length - 1];
+        } catch {
+            return 0;
+        }
+    }
+
+    function getEffectiveTokenBalance(
+        address routerAddress,
+        address token1,
+        address token2
+    ) private view returns (uint256) {
+        IUniswapV2Router02 router = IUniswapV2Router02(routerAddress);
+        address[] memory path = new address[](2);
+        path[0] = token1;
+        path[1] = token2;
+
+        try router.getAmountsOut(1e18, path) returns (
             uint256[] memory amounts
         ) {
             return amounts[amounts.length - 1];

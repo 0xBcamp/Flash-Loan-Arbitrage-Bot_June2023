3aa9ebdd489a21f9778b531c180eb28f66ab1204
Oleg Iaremchuk <oleg.iaremchuk@pm.me>
2023-07-23 10:01:02 +0200

    Whitelist dependencies in deploy script

--- contracts/scripts/deploy.ts
+++ contracts/scripts/deploy.ts
@@ -1,4 +1,10 @@
 import { ethers, network } from "hardhat";
+import {
+  ArbitrageBot,
+  ArbitrageExecutor,
+  ArbitrageFinder,
+  TradeExecutor,
+} from "../typechain-types";
 
 const {
   AAVE_POOL_ADDRESS_PROVIDER,
@@ -21,30 +27,25 @@ async function main() {
 }
 
 async function deployContracts() {
-  const tradeExecutorAddr = await deployTradeExecutor();
-  const arbitrageExecutorAddr = await deployArbitrageExecutor(
-    tradeExecutorAddr
+  const tradeExecutor = await deployTradeExecutor();
+  const arbitrageExecutor = await deployArbitrageExecutor(
+    await tradeExecutor.getAddress()
   );
-  const arbitrageFinderAddr = await deployArbitrageFinder();
+  const arbitrageFinder = await deployArbitrageFinder();
 
-  const botContractName = "ArbitrageBot";
-  console.log(`deploying ${botContractName} contract`);
-
-  const contractFactory = await ethers.getContractFactory(botContractName);
-
-  var contract = await contractFactory.deploy(
-    arbitrageFinderAddr,
-    arbitrageExecutorAddr
+  const arbitrageBot = await deployArbitrageBot(
+    await arbitrageFinder.getAddress(),
+    await arbitrageExecutor.getAddress()
   );
 
-  contract = await contract.waitForDeployment();
+  console.log("contracts were deployed, will begin whitelisting dependencies");
 
-  console.log(
-    `contract with name ${botContractName} was deployed to address: ${await contract.getAddress()}`
-  );
+  await arbitrageExecutor.addToWhitelist(await arbitrageBot.getAddress());
+  await arbitrageFinder.addToWhitelist(await arbitrageBot.getAddress());
+  await tradeExecutor.addToWhitelist(await arbitrageExecutor.getAddress());
 }
 
-async function deployTradeExecutor(): Promise<string> {
+async function deployTradeExecutor(): Promise<TradeExecutor> {
   const contractName = "TradeExecutor";
   console.log(`deploying ${contractName} contract`);
 
@@ -62,12 +63,12 @@ async function deployTradeExecutor(): Promise<string> {
     `contract with name ${contractName} was deployed to address: ${address}`
   );
 
-  return address;
+  return contract;
 }
 
 async function deployArbitrageExecutor(
   tradeExecutorAddr: string
-): Promise<string> {
+): Promise<ArbitrageExecutor> {
   const contractName = "ArbitrageExecutor";
   console.log(`deploying ${contractName} contract`);
 
@@ -85,10 +86,10 @@ async function deployArbitrageExecutor(
     `contract with name ${contractName} was deployed to address: ${address}`
   );
 
-  return address;
+  return contract;
 }
 
-async function deployArbitrageFinder(): Promise<string> {
+async function deployArbitrageFinder(): Promise<ArbitrageFinder> {
   const contractName = "ArbitrageFinder";
   console.log(`deploying ${contractName} contract`);
 
@@ -103,7 +104,32 @@ async function deployArbitrageFinder(): Promise<string> {
     `contract with name ${contractName} was deployed to address: ${address}`
   );
 
-  return address;
+  return contract;
+}
+
+async function deployArbitrageBot(
+  arbitrageFinderAddr: string,
+  arbitrageExecutorAddr: string
+): Promise<ArbitrageBot> {
+  const botContractName = "ArbitrageBot";
+  console.log(`deploying ${botContractName} contract`);
+
+  const contractFactory = await ethers.getContractFactory(botContractName);
+
+  var contract = await contractFactory.deploy(
+    arbitrageFinderAddr,
+    arbitrageExecutorAddr
+  );
+
+  contract = await contract.waitForDeployment();
+
+  const address = await contract.getAddress();
+
+  console.log(
+    `contract with name ${botContractName} was deployed to address: ${address}`
+  );
+
+  return contract;
 }
 
 main().catch((error) => {

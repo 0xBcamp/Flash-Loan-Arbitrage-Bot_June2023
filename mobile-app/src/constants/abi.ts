211d16953bed1fb9e28a960554caddb455875484
Oleg Iaremchuk <oleg.iaremchuk@pm.me>
2023-07-23 13:27:23 +0200

    mobile app updates

---
+++ mobile-app/src/constants/abi.ts
@@ -0,0 +1,94 @@
+export const botAbi = [
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_arbitragFinder",
+        type: "address",
+      },
+      {
+        internalType: "address",
+        name: "_arbitrageExecutor",
+        type: "address",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    anonymous: false,
+    inputs: [
+      {
+        indexed: true,
+        internalType: "bool",
+        name: "isFound",
+        type: "bool",
+      },
+    ],
+    name: "ArbitrageOpportunity",
+    type: "event",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_address",
+        type: "address",
+      },
+    ],
+    name: "addToWhitelist",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "token1",
+        type: "address",
+      },
+      {
+        internalType: "address",
+        name: "token2",
+        type: "address",
+      },
+    ],
+    name: "execute",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_address",
+        type: "address",
+      },
+    ],
+    name: "isWhitelisted",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_address",
+        type: "address",
+      },
+    ],
+    name: "removeFromWhitelist",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+];
--- mobile-app/src/constants/contract.ts
+++ mobile-app/src/constants/contract.ts
@@ -1,103 +1,14 @@
-// Arbitrage Engine Optimism Goerli network
-export const arbitrageBotAddress = "0xD21D93713ae9Bc83DA03360665799Ce95Ae61583";
-export const arbitrageFinderAddress =
-  "0xEEFe0B568Eb7572112DfFEdd8ACa99d8eeA65383";
-export const arbitrageExecutorAddress =
-  "0x56235F7cf1CC0F61685a077D7b1C8b7927838a09";
-export const tradeExecutorAddress =
-  "0x788018DA0c2656EC90494f55325d3dc862109857";
+export const GoerliBotAddress = "0xB4e2841F8DF81516D0F6318A1F1363E28BE92f33";
+export const GoerliFinderAddress = "0x55f892be15a649De649ecAB6A1f15DA7419A6aCB";
+export const GoerliExecutorAddress =
+  "0x3Dd35C630622dDb51c98c532F2f3e0fC68B20ad0";
+export const GoerliTradeExecutorAddress =
+  "0xBaEf66C5a73ea036ED135fcbFb59fcD00Fa50484";
 
-export const botAbi = [
-  {
-    inputs: [
-      {
-        internalType: "address",
-        name: "_arbitragFinder",
-        type: "address",
-      },
-      {
-        internalType: "address",
-        name: "_arbitrageExecutor",
-        type: "address",
-      },
-    ],
-    stateMutability: "nonpayable",
-    type: "constructor",
-  },
-  {
-    anonymous: false,
-    inputs: [
-      {
-        indexed: true,
-        internalType: "bool",
-        name: "isFound",
-        type: "bool",
-      },
-    ],
-    name: "ArbitrageOpportunity",
-    type: "event",
-  },
-  {
-    inputs: [
-      {
-        internalType: "address",
-        name: "_address",
-        type: "address",
-      },
-    ],
-    name: "addToWhitelist",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [
-      {
-        internalType: "address",
-        name: "token1",
-        type: "address",
-      },
-      {
-        internalType: "address",
-        name: "token2",
-        type: "address",
-      },
-    ],
-    name: "execute",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [
-      {
-        internalType: "address",
-        name: "_address",
-        type: "address",
-      },
-    ],
-    name: "isWhitelisted",
-    outputs: [
-      {
-        internalType: "bool",
-        name: "",
-        type: "bool",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [
-      {
-        internalType: "address",
-        name: "_address",
-        type: "address",
-      },
-    ],
-    name: "removeFromWhitelist",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-];
+export const MainnetBotAddress = "0x12652102b8Ca0A0559dE58185E8E1c4BEA25EA58";
+export const MainnetFinderAddress =
+  "0x14ac66b2ed679dA7b1b7b7Bf3cbd1Fb537F84037";
+export const MainnetExecutorAddress =
+  "0x6538c5EFa8Ef8D0E087E890b9b9f74CeB53FF72F";
+export const MainnetTradeExecutorAddress =
+  "0x889362Cb60eCCa8549034729666e905F411360C2";
